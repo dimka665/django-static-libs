01366945b88a6020ab3d81f508085b69759c0770
Dmitry Voronin <dimka665@gmail.com>
2013-09-09 20:34:08 +0400

    Fontello added

--- fabfile.py
+++ fabfile.py
@@ -2,4 +2,5 @@
 from fabric_env.fabfile import *
 
 
-environment.hg_path = 'git+ssh://git@github.com/dimka665/django-static-libs/'
+environment.hg_path = 'git+ssh://git@github.com:dimka665/django-static-libs.git'
+# environment.hg_path = 'git+https://github.com/dimka665/django-static-libs/'
